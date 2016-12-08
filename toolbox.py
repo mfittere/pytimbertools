8ec2a87aa9a3251e3da6c52d91026d62e791c0ba
mfittere <miriam.fitterer@cern.ch>
2016-12-08 14:41:00 -0600

    remove amplitude from Gaussian fit function

--- toolbox.py
+++ toolbox.py
@@ -3,8 +3,12 @@ import numpy as np
 def exp_fit(x,a,tau):
   return a*np.exp(x/tau)
 
-def gauss_fit(x,a,mean,sigma):
-  return a*np.exp(-((x-mean)**2/(2*sigma**2)))
+def gauss_fit(x,mean,sigma):
+  """
+  function for a normal or Gaussian distribution
+    f(x,mu,sig) = 1/(sqrt(2*sig**2*pi))*exp(-(x-mu)**2/(2*sig**2))
+  """
+  return 1/(np.sqrt(2*sigma**2*np.pi))*np.exp(-((x-mean)**2/(2*sigma**2)))
 
 def movingaverage(data,navg):
   """calculates the moving average over
